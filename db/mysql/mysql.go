905e057908a4539211f1a737cdbb8cd187304584
yu <yu.exclusive@icloud.com>
2021-12-26 00:11:22 +0800

    add generics

--- db/mysql/mysql.go
+++ db/mysql/mysql.go
@@ -3,8 +3,13 @@ package mysql
 import (
 	"github.com/yuexclusive/utils/db"
 	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func Init(dsn string, cfgs ...*db.Config) {
-	db.Init(mysql.Open(dsn), cfgs...)
+func Init(dsn string, name string, opts ...gorm.Option) {
+	db.Open(name, mysql.Open(dsn), opts...)
+}
+
+func InitDefault(dsn string, opts ...gorm.Option) {
+	db.Open("", mysql.Open(dsn), opts...)
 }
--- db/mysql/mysql_test.go
+++ db/mysql/mysql_test.go
@@ -1,11 +1,34 @@
 package mysql
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/yuexclusive/utils/db"
+	"github.com/yuexclusive/utils/db/dao"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
-func TestInit(t *testing.T) {
-	Init("test:123@tcp(127.0.0.1:3306)/evolve?charset=utf8mb4&parseTime=True&loc=Local")
+type Role struct {
+	ID          int64
+	Name        string
+	Description string
+}
 
-	// select {}
+func (r *Role) PK() int64 {
+	return r.ID
+}
+
+func TestInit(t *testing.T) {
+	InitDefault("test:123@tcp(127.0.0.1:30002)/evolve?charset=utf8mb4&parseTime=True&loc=Local", &gorm.Config{NamingStrategy: schema.NamingStrategy{SingularTable: true}})
+
+	d := dao.NewDao[Role](db.GetDefaultClient().DB)
+
+	d.Update(&Role{Description: "heh1"}, []dao.Where{{Query: "id=?", Args: []interface{}{1}}}, "Description")
+
+	data := d.Find(nil)
+
+	fmt.Println(data)
 }
