0822ca36dbc371f6f4ad965a7f60433554de4d69
yu <yu.exclusive@icloud.com>
2022-09-22 14:44:55 +0800

    fix

--- email/email_test.go
+++ email/email_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Test_Send(t *testing.T) {
-	dialer := NewDialer("smtp.gmail.com", 587, "xxx@gmail.com", "xxx")
+	dialer := NewDialer("smtp.gmail.com", 587, "evolve.publisher123@gmail.com", "")
 	err := dialer.Send("haha", "", "this is a fucking body", "xxx@icloud.com")
 	if err != nil {
 		t.Error(err)
